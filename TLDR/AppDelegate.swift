36e1dc075a4c7b2b5714201dd6d07b4f81d8e138
DeProdigy <alex.hint.ah@gmail.com>
2017-05-30 23:14:30 -0400

    Start the app with the results view if user data exists

--- TLDR/AppDelegate.swift
+++ TLDR/AppDelegate.swift
@@ -35,6 +35,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        // Show results if User data exists
+        if UserDefaults.standard.value(forKey: "date") != nil {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let view = storyboard.instantiateViewController(withIdentifier: "ResultsViewController") as! ResultsViewController
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            appDelegate.window?.rootViewController = view
+        }
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
